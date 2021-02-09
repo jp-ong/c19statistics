e34af7b272f25632756ab9fd17aeb149f5871780
John Paul Ong <jpong5202@gmail.com>
2021-02-09 09:45:10 +0800

    Added gtag scripts

--- pages/_document.js
+++ pages/_document.js
@@ -10,12 +10,23 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-0DN6M8WPJT"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'G-0DN6M8WPJT');
+        `,
+            }}
+          />
           <link rel="icon" href="/favicon.ico" />
           <link rel="stylesheet" href="https://use.typekit.net/czh2mrw.css" />
-          <script
-            async=""
-            src="https://www.googletagmanager.com/gtag/js?id=G-7TPYNBW9P2"
-          />
         </Head>
         <body>
           <Main />
