c410f5bfc8c8200fd0412d508ee53203dd02a892
John Paul Ong <jpong5202@gmail.com>
2021-03-11 12:32:37 +0800

    Added anchor to buttons

--- components/routerButtons.js
+++ components/routerButtons.js
@@ -1,32 +1,33 @@
 import { Tooltip, Button, Box } from "@material-ui/core";
 import { Public as PublicIcon, Home as HomeIcon } from "@material-ui/icons";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const RouterButtons = ({ homeButton, countryButton }) => {
   const router = useRouter();
   return (
     <Box display="flex" flexWrap="wrap" gridGap=".25em">
       <Tooltip title="Go to home page" arrow placement="top">
-        <Button
-          onClick={() => router.push("/")}
-          onMouseOver={() => router.prefetch("/")}
-          startIcon={<HomeIcon />}
-          variant="text"
-          disabled={!homeButton}
-        >
-          Home
-        </Button>
+        <Link href="/" passHref>
+          <Button
+            startIcon={<HomeIcon />}
+            variant="text"
+            disabled={!homeButton}
+          >
+            Home
+          </Button>
+        </Link>
       </Tooltip>
       <Tooltip title="Latest statistics of every country" arrow placement="top">
-        <Button
-          onClick={() => router.push("/latest")}
-          onMouseOver={() => router.prefetch("/latest")}
-          startIcon={<PublicIcon />}
-          variant="outlined"
-          disabled={!countryButton}
-        >
-          Countries
-        </Button>
+        <Link href="/latest" passHref>
+          <Button
+            startIcon={<PublicIcon />}
+            variant="text"
+            disabled={!countryButton}
+          >
+            Countries
+          </Button>
+        </Link>
       </Tooltip>
     </Box>
   );
